02e00c1b77d666c14af727e698b3bfe9cdf8c8d4
Pedro Guarnieri <pedroguarnieric@gmail.com>
2023-09-12 20:32:28 -0300

    Configuring Eureka clients

--- hr-payroll/src/main/java/com/example/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/entities/Worker.java
@@ -1,15 +1,43 @@
 package com.example.hrpayroll.entities;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@NoArgsConstructor
-@Getter
-@Setter
 
 public class Worker {
     private Long id;
     private String name;
     private Double dailyIncome;
+
+    public Worker(){
+
+    }
+
+    public Worker( String name, Double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
 }
--- hr-worker/src/main/java/com/example/hrworker/entities/Worker.java
+++ hr-worker/src/main/java/com/example/hrworker/entities/Worker.java
@@ -1,13 +1,9 @@
 package com.example.hrworker.entities;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@NoArgsConstructor
-@Getter
-@Setter
+import javax.persistence.*;
+
+
 @Entity
 @Table(name = "tb_worker")
 public class Worker {
@@ -16,4 +12,38 @@ public class Worker {
     private Long id;
     private String name;
     private Double dailyIncome;
+
+    public Worker(){
+
+    }
+
+    public Worker(Long id, String name, Double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
 }
