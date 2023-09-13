05c724d4c656b74233eda70c3e3be02c64112455
Pedro Guarnieri <pedroguarnieric@gmail.com>
2023-09-12 21:19:40 -0300

    Project hr-api-getaway-zull-created

---
+++ hr-api-getaway-zuul/src/main/java/com/example/hrapigetawayzuul/HrApiGetawayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrapigetawayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGetawayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGetawayZuulApplication.class, args);
+	}
+
+}
---
+++ hr-api-getaway-zuul/src/test/java/com/example/hrapigetawayzuul/HrApiGetawayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrapigetawayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGetawayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
